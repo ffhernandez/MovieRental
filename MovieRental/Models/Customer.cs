4425a30762f5aa0caca3f5bf14e65841fee2c857
Fiji Hernandez <fiji.hernandez@gmail.com>
2018-08-15 23:30:15 -0500

    Populated MembershipTypes table and Added CustomersController

--- MovieRental/Models/Customer.cs
+++ MovieRental/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,8 @@ namespace MovieRental.Models
     public class Customer
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public bool IsSubscribeToNewsLetter { get; set; }
         public byte MembershipTypeId { get; set; }
--- MovieRental/Models/Movie.cs
+++ MovieRental/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace MovieRental.Models
     public class Movie
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
     }
 }
